57cf54ddebf915c699518c016f48a479af0a7b86
Xuyang Li <li5274@purdue.edu>
2024-11-17 15:42:07 -0500

    copy paste to guardedInsert

--- src/synthesiser/Synthesiser.cpp
+++ src/synthesiser/Synthesiser.cpp
@@ -1751,24 +1751,30 @@ void Synthesiser::emitCode(std::ostream& out, const Statement& stmt) {
             out << "Tuple<RamDomain," << arity << "> tuple{{" << join(guardedInsert.getValues(), ",", rec)
                 << "}};\n";
 
+            auto tempRelName = rel->getName();
             // insert tuple
             out << relName << "->"
                 << "insert(tuple," << ctxName << ");\n";
-            auto tempRelName = rel->getName();
 
-            // record derivation info about realTuple
-            // if target rel is @delta or @new, avoid record new derivation
-            // get the tuple from the relation; it might be old
-            if ((!(guardedInsert.getClauseStr() == "UNKNOWN CLAUSE")) // TODO
-                && (!(tempRelName.size() >= 6 && tempRelName.substr(0, 6) == "@delta"))) {
+            // insert tuple and record derivation
+            // case 1: insert to delta, (new) to old
+            if (guardedInsert.getClauseStr() == "UNKNOWN CLAUSE") {
+                out << relName << "->"
+                    << "insert(tuple," << ctxName << ");\n";
+            } else {
+                // case 2: insert to new/original rel, record derivation
                 if (tempRelName.size() >= 4 && tempRelName.substr(0, 4) == "@new") {
                     tempRelName.erase(tempRelName.begin(), tempRelName.begin() + 5);  // there is an extra '_'
                 }
+                // retrieve the tuple first
                 out << "auto untypedTuple = UntypedTuple::fromTypedTuple(\"" << tempRelName << "\",tuple);\n";
                 out << "auto*& ruleSet = DerivationManager::untypedTuple2Rules[untypedTuple];\n";
                 out << "if (ruleSet == nullptr) {\n";
+                out << relName << "->"
+                    << "insert(tuple," << ctxName << ");\n";  // only insert tuple to rel when it wasn't recorded
                 out << "ruleSet = new std::set<souffle::RamDomain>();\n";
                 out << "}\n";
+                // record derivation info about realTuple
                 out << "ruleSet->insert(" << guardedInsert.getClauseID() <<");\n";
             }
             // end of conseq body.
