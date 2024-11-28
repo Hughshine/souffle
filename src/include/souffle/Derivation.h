aafc7d66e3876e01f7e26e05a2517b08e2b5b4f9
Xuyang Li <li5274@purdue.edu>
2024-11-28 01:57:25 -0500

    Now support dump to and read from json format derivations

--- src/include/souffle/Derivation.h
+++ src/include/souffle/Derivation.h
@@ -5,6 +5,7 @@
 #ifndef DERIVATION_H
 #define DERIVATION_H
 #include "souffle/RamTypes.h"
+#include "utility/json11.h"
 
 #include <cassert>
 #include <fstream>
@@ -18,6 +19,22 @@ struct UntypedTuple {
     static std::string toString(const UntypedTuple& tuple) {
         std::string result = tuple.relation_name + '(' + toStringFields(tuple.fields) + ')';
         return result;
+    }
+
+    std::string toString() const {
+        std::string result = relation_name + '(' + toStringFields(fields) + ')';
+        return result;
+    }
+
+    json11::Json toJson() const {
+        // json11::Json::array fields = json11::Json::array();
+        // for (const auto& field : fields) {
+        //     fields.emplace_back(field);
+        // }
+        return json11::Json::object{
+            {"rel", relation_name},
+            {"fields", json11::Json::array(fields.begin(), fields.end())}
+        };
     }
     static std::string toStringFields(const std::vector<souffle::RamDomain>& fields) {
         std::string result;
@@ -61,7 +78,8 @@ struct UntypedTuple {
     }
 };
 
-inline UntypedTuple testUntypedTuple{"T", {0, -1, -2, -42}};
+inline UntypedTuple testUntypedTuple1{"T", {0, -1, -2, -42}};
+inline UntypedTuple testUntypedTuple2{"S", {0, 1, 2, 42}};
 
 struct RuleApplication {
     souffle::RamDomain ruleId;
@@ -100,19 +118,54 @@ struct RuleApplication {
         }
         return false;
     }
+
+    json11::Json toJson() const {
+        json11::Json::array mapping;
+        for (const auto& [var, value] : varValues) {
+            mapping.emplace_back(
+                json11::Json::object{{var, (value)}}
+            );
+        }
+        json11::Json result = json11::Json::object{
+            {
+                {"ruleId", ruleId},
+                {"mapping", mapping},
+            }
+        };
+        return result;
+    }
 };
 
 /** Fact is trivially true; use the naiveRuleApplication for such cases when needed */
 inline RuleApplication naiveRuleApplication{0, {}};
 
 inline std::map<std::string, souffle::RamDomain> testVarValues = {{"x", 1}, {"y", 2}};
-inline RuleApplication testRuleApplication{47906, testVarValues};
+inline RuleApplication testRuleApplication1{1, testVarValues};
+inline RuleApplication testRuleApplication2{2, testVarValues};
+inline RuleApplication testRuleApplication3{3, testVarValues};
+inline RuleApplication testRuleApplication4{114514, testVarValues};
+
+inline std::set<RuleApplication> testRuleApplicationSet1{
+    testRuleApplication1, testRuleApplication2,
+    testRuleApplication3, testRuleApplication4,
+};
+
+inline std::set<RuleApplication> testRuleApplicationSet2{
+    testRuleApplication4, testRuleApplication3,
+    testRuleApplication2, testRuleApplication1,
+};
+
+// testDerivationInfo
+inline std::map<UntypedTuple, std::set<RuleApplication>*> testUntypedTuple2RuleApplications{
+    {testUntypedTuple1, &testRuleApplicationSet1},
+    {testUntypedTuple2, &testRuleApplicationSet2},
+};
+
 
 class DerivationManager {
 public:
 // private:
     // TODO
-    // mapping from tuple's pomainter to rules that derive it
     static inline std::set<souffle::RamDomain> testRules = {
         0, 1, 2, 42
     };
@@ -142,6 +195,90 @@ public:
         }
         return result + ']';
     }
+
+    static json11::Json ruleApp2Json(const RuleApplication& ruleApp) {
+        json11::Json result = json11::Json();
+        return result;
+    }
+    /**
+     * Derivation Info: {A(1)	[1[x->1],2[x->1]]}
+     * JSON: [{}]
+     */
+    static json11::Json derivationInfo2Json(const std::map<UntypedTuple, std::set<RuleApplication>*>& derivationInfo) {
+        json11::Json::array result;
+        for (const auto& [tuple, ruleApps]: derivationInfo) {
+            json11::Json::array ruleAppsJson = json11::Json::array();
+            for (const auto& ruleApp: *ruleApps) {
+                ruleAppsJson.push_back(ruleApp.toJson());
+            }
+            json11::Json item = json11::Json::object{
+                {"tuple", json11::Json::object{
+                    {"rel", tuple.relation_name},
+                    {"fields", json11::Json::array(tuple.fields.begin(), tuple.fields.end())}}
+                },
+                {"edge", ruleAppsJson}
+            };
+            result.emplace_back(item);
+        }
+        return result;
+    }
+
+    static std::map<UntypedTuple, std::set<RuleApplication>*> derivationInfoFromJson(const json11::Json& infoJson) {
+        std::map<UntypedTuple, std::set<RuleApplication>*> derivationInfo;
+        for (const auto& item: infoJson.array_items()) {
+            std::vector<souffle::RamDomain> fields;
+            for (const auto& field: item["tuple"]["fields"].array_items()) {
+                fields.push_back(field.int_value());
+            }
+            UntypedTuple tuple{item["tuple"]["rel"].string_value(), fields};
+            if (derivationInfo.count(tuple) == 0) {
+                derivationInfo[tuple] = new std::set<RuleApplication>();
+            }
+            std::set<RuleApplication>* ruleApps = derivationInfo[tuple];
+            for (const auto& ruleAppJson: item["edge"].array_items()) {
+                RuleApplication ruleApp;
+                souffle::RamDomain ruleId = ruleAppJson["ruleId"].int_value();
+                ruleApp.ruleId = ruleId;
+                for (const auto& map: ruleAppJson["mapping"].array_items()) {
+                    for (const auto& [var, value]: map.object_items()) { // should be only one item here
+                        ruleApp.varValues[var] = value.int_value();
+                    }
+                }
+                ruleApps->insert(ruleApp);
+            }
+            derivationInfo[tuple] = ruleApps;
+        }
+        // parse json to map
+        return derivationInfo;
+    }
+
+
+    static void derivationInfo2JsonFile(const std::string& originalFileName, const std::string& suffix, const std::map<UntypedTuple, std::set<RuleApplication>*>& derivationInfo) {
+        json11::Json result = derivationInfo2Json(derivationInfo);
+
+        std::ofstream outputFile(
+            suffix.empty()
+                ? originalFileName + ".json"
+                : originalFileName + "." + suffix + ".json"
+            );
+
+        outputFile << result.dump();
+    }
+
+
+    static std::map<UntypedTuple, std::set<RuleApplication>*> derivationInfoFromJsonFile(const std::string& originalFileName, const std::string& suffix) {
+        std::ifstream inputFile((suffix.empty() ? originalFileName : originalFileName + "." + suffix ) + ".json");
+        std::string infoString = std::string((std::istreambuf_iterator<char>(inputFile)),
+                       std::istreambuf_iterator<char>());;
+        std::string err;
+        json11::Json infoJson = json11::Json::parse(infoString, err);
+        assert (err.empty() && "Json parse error");
+        return std::move(derivationInfoFromJson(infoJson));
+    }
+
+    // static void dumpDerivationInfo2JsonFile(const std::string& originalFileName, const std::string& suffix, const std::set<RuleApplication>* ruleApplications) {
+    //
+    // }
 
     static void dumpDerivationInfo(const std::string& filename, const std::string& outputDir) {
         // TODO: should change to souffle's IO system later
