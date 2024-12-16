60fd95c1296fa0499b3b87ea31410db6102b1876
Xuyang Li <li5274@purdue.edu>
2024-12-15 20:50:46 -0500

    test, trivial

--- tests/non_recursion/NstratumNclause/test.sh
+++ tests/non_recursion/NstratumNclause/test.sh
@@ -15,20 +15,31 @@ echo "Building incremental compilation" &
 ../../../cmake-build-debug/src/souffle test.dl --inc -F./input -D./output -o test-incr
 
 ### 3. run full and check its result
-#echo "Running full compilation" &
-#./test-full
-#if cmp -s test.dl.json result/test.dl.json.full; then
-#  echo "SUCCESS: Full compilation" &
-#else
-#  echo "FAIL: Full compilation" &
-#fi
-#
-## 4. run incr and check its result
-#echo "Running incr compilation" &
-#./test-incr
-#if cmp -s test.dl.json result/test.dl.json.incr; then
-#  echo "SUCCESS: incremental compilation" &
-#else
-#  echo "FAIL: incremental compilation" &
-#fi
-#
+echo "Running full compilation" &
+./test-full
+if cmp -s test.dl.json result/test.dl.json.full; then
+  echo "SUCCESS: Full compilation" &
+else
+  echo "FAIL: Full compilation" &
+fi
+
+# 4. run incr and check its result
+echo "Running incr compilation" &
+./test-incr
+if cmp -s test.dl.json result/test.dl.json.incr; then
+  echo "SUCCESS: incremental compilation, derivations" &
+else
+  echo "FAIL: incremental compilation, derivations" &
+fi
+
+if cmp -s test.dl.insert.json result/test.dl.insert.json.incr; then
+  echo "SUCCESS: incremental compilation, inserted derivations" &
+else
+  echo "FAIL: incremental compilation, inserted derivations" &
+fi
+
+if cmp -s test.dl.delete.json result/test.dl.delete.json.incr; then
+  echo "SUCCESS: incremental compilation, deleted derivations" &
+else
+  echo "FAIL: incremental compilation, deleted derivations" &
+fi
