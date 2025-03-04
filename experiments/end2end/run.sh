060c3ddbdb18784d5ea93b2f83252e36c6b6c846
Xuyang Li <li5274@purdue.edu>
2025-03-04 12:41:10 -0500

    a basic end2end example

--- experiments/end2end/run.sh
+++ experiments/end2end/run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-# Compiler configuration
-COMPILER="/usr/bin/c++"
-INCLUDES="-I/home/hugh/research/datalog/souffle/src/include -I/usr/include"
-STD_FLAG="-std=c++17"
-CXX_FLAGS="-fopenmp"
-DEBUG_FLAGS="-g"
-DEFINITIONS="-DUSE_NCURSES -DUSE_LIBZ -DUSE_SQLITE"
-LINK_OPTIONS="-ldl /usr/lib/x86_64-linux-gnu/libsqlite3.so /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/x86_64-linux-gnu/libncurses.so"
-RPATH="-Wl,-rpath,/usr/lib/x86_64-linux-gnu"
-
-# Compile command
-$COMPILER $INCLUDES $STD_FLAG $CXX_FLAGS $DEBUG_FLAGS $DEFINITIONS \
-    main.cpp DerivationGraph.cpp Rule.cpp $LINK_OPTIONS $RPATH -o test
+python ../../cmake-build-debug/src/souffle-compile.py main.cpp -o test --with-cudd -v
 
 rm -f *.o
 
