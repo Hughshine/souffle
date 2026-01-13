0abe19111a1d1cc93ca080dd79e8d4eb8e2fe108
Xuyang Li <li5274@purdue.edu>
2026-01-13 14:41:37 -0500

    fix(problog): propagate fc-profile/post-del flags

--- src/problog/Pipeline.cpp
+++ src/problog/Pipeline.cpp
@@ -1510,6 +1510,8 @@ void runPipeline(
         bool enableOnlineCli) {
     std::cout << std::fixed << std::setprecision(8);
     Debugger& debugger = Debugger::getInstance();
+    fcProfileEnabled = opt.isFcProfileEnabled();
+    postDelEnabled = opt.isPostDelEnabled();
     DerivationGraphViewInterface::setDumpDotEnabled(opt.isDumpDotEnabled());
     DerivationGraphViewInterface::setDumpJsonEnabled(opt.isDumpJsonEnabled());
     DerivationGraphViewInterface::setDumpStatsEnabled(opt.isDumpStatEnabled());
